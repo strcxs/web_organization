e23de767558545ae2d650d7ff346100c4d35144b
strcxs <rdf.faishal@gmail.com>
2024-03-24 11:24:37 +0700

    update title

--- resources/views/discuss.blade.php
+++ resources/views/discuss.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | User Profile</title>
+  <title>Dashboard • Discuss</title>
   @include('include/link')
   <style>
     .card {
--- resources/views/visimisi.blade.php
+++ resources/views/visimisi.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | User Profile</title>
+  <title>Dashboard • Content</title>
   @include('include/link')
 </head>
 <body class="hold-transition light-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
